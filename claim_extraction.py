87b87352bb40fdf00239a526691411e6f0bbe639
YevhenKost <kosteugeneo@gmail.com>
2024-02-01 14:25:34 +0000

    added some docs for claim extr

--- claim_extraction.py
+++ claim_extraction.py
@@ -14,7 +14,6 @@ class RelationClaimExtractor:
         """
         Construct a meta json for the unique nodes for convenience.
 
-
         :param aif_json: AIF formatted json, with the key "nodes"
         :return: --dict:
             {
@@ -27,7 +26,6 @@ class RelationClaimExtractor:
              'text': 'Our schools are really unsafe due to gun laws',
              'timestamp': '2020-05-28 19:24:33',
              'type': 'I'
-
              }
         """
 
@@ -39,22 +37,40 @@ class RelationClaimExtractor:
 
     @classmethod
     def get_claim_nodes_xaif(cls, xaif_json: Dict[str, Any]) -> List[Dict[str, str]]:
+        """Run extraction on xAIF format. Input is a dict with the key "AIF" that has AIF formatted json.
+        For output, see get_claim_nodes_aif"""
         return cls.get_claim_nodes_aif(aif_json=xaif_json["AIF"])
 
     @classmethod
     def get_claim_texts_aif(cls, aif_json: Dict[str, Any]) -> List[str]:
+        """
+        Retrieve claims from AIF data sample
+
+        :param aif_json: dict, AIF formatted dict.
+        :return: list of str, texts of retrieved claims
+        """
         nodes_dicts = cls.get_claim_nodes_aif(aif_json=aif_json)
         claims_texts = [node_dict["text"] for node_dict in nodes_dicts]
         return claims_texts
 
     @classmethod
     def get_claim_texts_xaif(cls, xaif_json: Dict[str, Any]) -> List[str]:
+        """
+        Retrieve claims from xAIF data sample.
+        Input: xAIF data sample. Should have key "AIF" with AIF dict attached to it.
+        return: see get_claim_texts_aif
+        """
         nodes_dicts = cls.get_claim_nodes_xaif(xaif_json=xaif_json)
         claims_texts = [node_dict["text"] for node_dict in nodes_dicts]
         return claims_texts
 
     @classmethod
     def get_claim_nodes_aif(cls, aif_json: Dict[str, Any]) -> List[Dict[str, str]]:
+        """
+
+        :param aif_json:
+        :return:
+        """
         keep_nodes = []
 
         node_meta_dict = cls._get_meta_nodes_dict(aif_json)
